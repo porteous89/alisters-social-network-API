0838562b7b01914b2916da3b4e8075a1f376cd2e
Alister Porteous <alisterporteous@hotmail.com>
2023-03-14 16:32:45 -0400

    Created Reaction model subdocument, created reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const {Schema, model, Types} = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+
+module.exports = reactionSchema
