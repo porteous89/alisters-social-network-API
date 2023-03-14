ce5802991a7177e677433bf41af3c6c0c1df7c69
Alister Porteous <alisterporteous@hotmail.com>
2023-03-14 14:56:21 -0400

    Initial commit, started folder setup, added User and Thought models, created config/connection file

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// get total count of friends on retrieval
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
