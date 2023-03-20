b43e0068862d559df50ebf52c6f8fa2ff5228cfb
Alister Porteous <alisterporteous@hotmail.com>
2023-03-20 11:00:15 -0400

    added addFriend and removeFriend put and delete routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,16 +5,22 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/userController.js');
 
-// /api/courses
+// /api/users
 router.route('/').get(getUsers).post(createUser);
 
-// /api/courses/:courseId
+// /api/users/:userId
 router
   .route('/:userId')
   .get(getSingleUser)
   .put(updateUser)
   .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').put(addFriend).delete(removeFriend);
+
 
 module.exports = router;
